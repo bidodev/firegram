0e726f6ec96352170442d857855abd9155977a56
Claudinei Bido <bido.c@live.com>
2020-12-09 11:25:37 +0100

    useStorage hook

--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+  const [stateFile, setStateFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ['image/png', 'image/jpeg'];
+
   const onLoadFile = (event) => {
-    console.log(event);
+    const [file] = event.target.files;
+
+    if (file && types.includes(file.type)) {
+      setStateFile(file);
+      if (error) {
+        setError(null);
+      }
+    } else {
+      setStateFile(null);
+      setError('Please select a valid image file');
+    }
   };
 
   return (
     <form>
-      <input type="file" onChange={onLoadFile} />
+      <label htmlFor="upload">
+        <input type="file" accept="image/*" onChange={onLoadFile} />
+        <span>+</span>
+      </label>
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+        {stateFile && <div>{stateFile.name}</div>}
+      </div>
     </form>
   );
 };
